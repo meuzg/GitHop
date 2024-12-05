c358087f61afb4d4130adf1670444cb93dabbf47
Meuz <gg.me.code@outlook.com>
2024-12-05 13:27:08 +0700

    Add Hopper class with initial method

---
+++ GitHop/Hopper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace GitHop
+{
+    public static class Hopper
+    {
+        public static async Task<List<string>> GetBranchesAsync()
+        {
+            // Initialize result list
+            var branches = new List<string>();
+            // Define process info
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = "branch --list",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            // Initialize process
+            using (var process = new Process())
+            {
+                // Initial process properties
+                process.StartInfo = startInfo;
+                // Start process
+                process.Start();
+                // Obtain result from process output
+                var output = await process.StandardOutput.ReadToEndAsync();
+                // Wait for process to exit with timeout
+                await process.WaitForExitAsync();
+                // Get branches info
+                var branchList = output.Split(['\n', '\r'], StringSplitOptions.RemoveEmptyEntries);
+                // Loop for each branch ...
+                foreach (var branch in branchList)
+                {
+                    // Trim whitespace and asterisk (which indicates the current branch)
+                    branches.Add(branch.Trim().TrimStart('*').Trim());
+                }
+            }
+            // After all, return branch list
+            return branches;
+        }
+    }
+}
