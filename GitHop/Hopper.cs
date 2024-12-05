61160b3c8acc9f4468d642776bcfff80b5a42b6a
Meuz <gg.me.code@outlook.com>
2024-12-05 13:30:59 +0700

    Add GetCurrentBranchNameAsync method

--- GitHop/Hopper.cs
+++ GitHop/Hopper.cs
@@ -46,5 +46,49 @@ namespace GitHop
             // After all, return branch list
             return branches;
         }
+
+        public static async Task<string> GetCurrentBranchNameAsync()
+        {
+            // Initialize result
+            string currentBranch = "";
+            // Define process info
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = "branch --list",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            // Initialize process
+            using (var process = new Process())
+            {
+                // Initial process properties
+                process.StartInfo = startInfo;
+                // Start process
+                process.Start();
+                // Obtain result from process output
+                var output = await process.StandardOutput.ReadToEndAsync();
+                // Wait for process to exit with timeout
+                await process.WaitForExitAsync();
+                // Get branches info
+                var branchList = output.Split(['\n', '\r'], StringSplitOptions.RemoveEmptyEntries);
+                // Loop for each branch ...
+                foreach (var branch in branchList)
+                {
+                    // If asterisk was found ...
+                    if (branch.StartsWith('*'))
+                    {
+                        // Assign current branch name
+                        currentBranch = branch.Trim().TrimStart('*').Trim();
+                        // Then exit this loop
+                        break;
+
+                    }
+                }
+            }
+            // After all, return the current branch name
+            return currentBranch;
+        }
     }
 }
